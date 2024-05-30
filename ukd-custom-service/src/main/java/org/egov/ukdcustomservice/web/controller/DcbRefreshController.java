08017ef1b6580516b3a9b805c7adc51585f8860e
Rohit Rai <rohit0235@gmail.com>
2024-05-30 17:09:43 +0530

    Update DcbRefreshController.java
    Add a controller for the Demand Auto Generate.

--- ukd-custom-service/src/main/java/org/egov/ukdcustomservice/web/controller/DcbRefreshController.java
+++ ukd-custom-service/src/main/java/org/egov/ukdcustomservice/web/controller/DcbRefreshController.java
@@ -3,14 +3,17 @@ package org.egov.ukdcustomservice.web.controller;
 import java.util.List;
 
 import org.egov.ukdcustomservice.service.DcbRefreshService;
+import org.egov.ukdcustomservice.service.AutomaticDemandGenerationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import lombok.extern.slf4j.Slf4j;
@@ -21,6 +24,9 @@ public class DcbRefreshController {
 
     @Autowired
     private DcbRefreshService dcbRefreshService;
+
+    @Autowired
+    private AutomaticDemandGenerationService automaticDemandGenerationService;
 
     @RequestMapping(value = "/v1/pt/dcb/_refresh", method = RequestMethod.GET )
     public ResponseEntity<Object> method() {
@@ -41,5 +47,16 @@ public class DcbRefreshController {
        // httpHeaders.setLocation(UriComponentsBuilder.fromHttpUrl(defaultURL).build().encode().toUri());
         return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
     }
+
+    //for demand Autogenerate
+    
+    @RequestMapping(value = "/v1/pt/demandAutoGenerate", method = RequestMethod.GET)
+    @Scheduled(fixedRate = 50000)
+	public ResponseEntity<Object> demandAutoGenerate() {
+
+		HttpHeaders httpHeaders = new HttpHeaders();
+		automaticDemandGenerationService.AutomaticDemandGeneration();
+		return new ResponseEntity<>(httpHeaders, HttpStatus.FOUND);
+	}
 
 }
