92c48c0e2bed3736785aa30aeeba9fe3881da9f4
Rohit Rai <rohit0235@gmail.com>
2024-05-30 17:20:13 +0530

    Update UkdcustomserviceApplication.java regarding the scheduling import
    import org.springframework.scheduling.annotation.EnableScheduling;

--- ukd-custom-service/src/main/java/org/egov/ukdcustomservice/UkdcustomserviceApplication.java
+++ ukd-custom-service/src/main/java/org/egov/ukdcustomservice/UkdcustomserviceApplication.java
@@ -11,6 +11,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling	
